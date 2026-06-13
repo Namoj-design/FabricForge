b08df8a885e5e71b7f75bc7398c863f995569b5a
Namoj-design <namojperiakumar@gmail.com>
2026-06-13 22:54:28 +0530

    miscellaneous fixing in codebase

---
+++ templates/chaincode/go/asset.go
@@ -0,0 +1,10 @@
+package go
+package main
+
+type Asset struct {
+	ID             string `json:"id"`
+	Color          string `json:"color"`
+	Size           int    `json:"size"`
+	Owner          string `json:"owner"`
+	AppraisedValue int    `json:"appraisedValue"`
+}
---
+++ templates/chaincode/go/go.mod
@@ -0,0 +1,5 @@
+module fabricforge-chaincode
+
+go 1.24
+
+require github.com/hyperledger/fabric-contract-api-go v1.2.2
